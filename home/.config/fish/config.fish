9f3a529874e408dd19ec46a187de29a75ac43e3d
Fred Deschenes <tifred2004@gmail.com>
2016-04-24 19:29:12 -0400

    Cleanup config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,17 +7,32 @@ function R
     echo '(R)'
 end
 
-# TODO: Check if keychain is installed before running this
-keychain --eval --quiet --nogui --quick | sed 's/[[:space:]]\?and//g' | source
+function command_exists
+    command --search $argv[1]>/dev/null
+end
+
+command_exists keychain; and begin
+    keychain --eval --quiet --nogui --quick | sed 's/[[:space:]]\?and//g' | source
+end; or begin
+    echo "'keychain' is not installed."
+end
 
 set -gx PATH ~/bin /opt/local/bin /opt/local/sbin /usr/local/sbin $PATH
 
+# TODO: Find a way to efficiently check if virtualfish is installed
 eval (python -m virtualfish)
 
 # Load prm-fish and setup completions
-. ~/dev/prm-fish/prm.fish
-emit prm_setup
+if not test -d ~/dev/prm-fish/
+    git clone git@github.com:FredDeschenes/prm-fish.git ~/dev/prm-fish
+    echo "Installed 'prm-fish'."
+end
+source ~/dev/prm-fish/prm.fish
 
 # Load homeshick + completions
-source "$HOME/.homesick/repos/homeshick/homeshick.fish"
-source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
+if not test -d ~/.homesick/repos/homeshick/
+    git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+    echo "Installed 'homeshick'."
+end
+source ~/.homesick/repos/homeshick/homeshick.fish
+source ~/.homesick/repos/homeshick/completions/homeshick.fish
